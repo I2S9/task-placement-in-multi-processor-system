bc744c3a88ebf0a3cb4ade8aead4e1e549fcdb8e
I2S9 <111307883+I2S9@users.noreply.github.com>
2023-12-27 14:26:35 +0100

    Update functions.c

--- main/functions.c
+++ main/functions.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include "functions.h"
 #include "structures.h"
+#include <time.h>
+#define percentage 0.3
 
 void loadData(char* filename, int* nbProc, int* nbTask, int*** execCost, int*** commCost)
 {
@@ -54,8 +56,7 @@ taskRepartition* getRandomTaskRepartition(int nbTask, int nbProc) {
     return (tr);
 }
 
-double getTotalExecCost(taskRepartition *tr, double **execCost,
-        double **commCost, int nbTask, int nbProc) {
+double getTotalExecCost(taskRepartition *tr, int **execCost, int **commCost, int nbTask, int nbProc) {
     double cost;
     double *costByProc = malloc(nbProc * sizeof(double));
     for (int p = 0; p < nbProc; p++) {
@@ -85,4 +86,63 @@ double getTotalExecCost(taskRepartition *tr, double **execCost,
     return (cost);
 }
 
+void sortPopulation(taskRepartition** Population, int size)
+{
+ for (int i = 1; i < size; i++) {
+        taskRepartition* current = Population[i];
+        int j = i - 1;
+        while (j >= 0 && Population[j]->totalCost > current->totalCost) {
+            Population[j + 1] = Population[j];
+            j = j - 1;
+        }
+        Population[j + 1] = current;
+    } 
+}
 
+void displayPopulation(taskRepartition** population, int size, int nbTask) {
+    printf("Affichage de la population :\n");
+    for (int i = 0; i < size; i++) {
+        printf("Individu %d: ", i);
+        for (int j = 0; j < nbTask; j++) {
+            printf("%d ", population[i]->procRepartition[j]);
+        }
+        printf("\t Coût total : %f\n", population[i]->totalCost);
+    }
+}
+
+taskRepartition* cross(taskRepartition* parent1, taskRepartition* parent2, int nbTask) {
+    taskRepartition* child = malloc(sizeof(taskRepartition));
+    child->procRepartition = malloc(nbTask * sizeof(int));
+
+    int pivot = rand() % nbTask;
+    for (int i = 0; i < pivot; i++) {
+        child->procRepartition[i] = parent1->procRepartition[i];
+    }
+    for (int i = pivot; i < nbTask; i++) {
+        child->procRepartition[i] = parent2->procRepartition[i];
+    }
+    child->totalCost = 0;
+    return child;
+}
+
+
+void initializePopulation(taskRepartition** Population, int size, int **execCost, int commCost, int nbProc, int nbTask, double percentage)
+{
+    for (int i = 0; i < size; i++) {
+        Population[i] = getRandomTaskRepartition(nbTask, nbProc);
+        Population[i]->totalCost = getTotalExecCost(Population[i], **execCost, **commCost, nbTask, nbProc);
+    }
+    while (Population[0]->totalCost > percentage) 
+    {
+        sortPopulation(Population, size);
+
+        int numSurvivors = (2 * size) / 3;
+
+        for (int i = numSurvivors; i < size; i++) {
+            int parent1Idx = rand() % numSurvivors; 
+            int parent2Idx = rand() % size; 
+        }
+
+    }
+    printf("Solution trouvée dans la population avec un coût de %f\n", Population[0]->totalCost);
+}
