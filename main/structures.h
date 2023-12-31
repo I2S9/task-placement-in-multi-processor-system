755339ab98a0595531f7225ba312dd946b98994f
I2S9 <111307883+I2S9@users.noreply.github.com>
2023-12-31 17:50:38 +0100

    Update structures.h

--- main/structures.h
+++ main/structures.h
@@ -10,5 +10,9 @@ struct taskRepartition{
 
 typedef taskRepartition* taskPopulation;
 
+#define POPULATION_SIZE 10000
+#define SELECTION_RATE 66
+#define CROSS_RATE 40
+#define MUTATION_RATE 20
 
 #endif // STRUCTURES_H
