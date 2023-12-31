4d454e8cb18b9208de9e384e121ee513dc6c9602
I2S9 <111307883+I2S9@users.noreply.github.com>
2023-12-31 11:06:35 +0100

    Update functions.h

--- main/functions.h
+++ main/functions.h
@@ -1,5 +1,15 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+#include "structures.h"
+
 void loadData(char* filename, int* nbProc, int* nbTask, int*** execCost, int*** commCost);
 void displayExecCost(int **execCost, int nbProc, int nbTask);
 void displaycommCost(int **commCost, int nbProc, int nbTask);
 taskRepartition* getRandomTaskRepartition(int nbTask, int nbProc);
-int getTotalExecCost(taskRepartition *tr, int **execCost,int **commCost, int nbTask);
+double getTotalExecCost(taskRepartition *tr, int **execCost,int **commCost, int nbTask, int nbProc);
+
+void initializePopulation(taskRepartition** Population, int size, int nbTask, int nbProc,int **execCost, int **commCost);
+taskRepartition* getRandomTaskRepartition(int nbTask, int nbProc);
+double getTotalExecCost(taskRepartition *tr, int **execCost,int **commCost, int nbTask, int nbProc);
+
+#endif // FUNCTIONS_H
