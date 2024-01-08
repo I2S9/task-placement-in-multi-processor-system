9ce27a4cc6ea4d494909094202bc5ab5746d5fee
Inès Gbadamassi <111307883+I2S9@users.noreply.github.com>
2024-01-08 22:33:13 +0100

    Update main.c

--- main/main.c
+++ main/main.c
@@ -10,7 +10,7 @@
 #define SELECTION_RATE 66
 #define CROSS_RATE 40
 #define MUTATION_RATE 20
-#define NOMBRE_GENERATIONS 100 
+#define NOMBRE_GENERATIONS 14
 
 typedef struct taskRepartition taskRepartition;
 
@@ -20,8 +20,6 @@ struct taskRepartition{
 };
 
 typedef taskRepartition* taskPopulation;
-
-
 
 void loadData(char* filename, int* nbProc, int* nbTask, int*** execCost, int*** commCost);
 void displayExecCost(int **execCost, int nbProc, int nbTask);
@@ -69,7 +67,9 @@ int main(void)
         return 1;
     }
     for (int i = 0; i < POPULATION_SIZE; i++) {
-        currentPopulation[i] = getRandomTaskRepartition(nbTask, nbProc); 
+        currentPopulation[i] = getRandomTaskRepartition(nbTask, nbProc);
+        currentPopulation[i]->totalCost = getTotalExecCost(currentPopulation[i], execCost,
+                commCost, nbTask, nbProc);
     }
 
     for (int generation = 0; generation < NOMBRE_GENERATIONS; generation++) {
@@ -245,12 +245,20 @@ void displayPopulation(taskRepartition **population, int taillePopulation) {
     }
 }
 
+// void mutate(taskRepartition* individu, int nbTask, int nbProc) {
+//     int taskToMutate = rand() % nbTask;
+//     int newProc = rand() % nbProc;
+//     individu->procRepartition[taskToMutate] = newProc;
+// }
+
 void mutate(taskRepartition* individu, int nbTask, int nbProc) {
-    for (int i = 0; i < nbTask; i++) {
-        if (rand() % 100 < MUTATION_RATE) {
-            individu->procRepartition[i] = rand() % nbProc;
-        }
-    }
+    int taskToMutate = rand() % nbTask;
+    int newProc;
+    do {
+        newProc = rand() % nbProc;
+    } while (newProc == individu->procRepartition[taskToMutate]);
+    
+    individu->procRepartition[taskToMutate] = newProc;
 }
 
 taskRepartition* cross(taskRepartition* parent1, taskRepartition* parent2, int nbTask) {
@@ -263,6 +271,39 @@ taskRepartition* cross(taskRepartition* parent1, taskRepartition* parent2, int n
 
     return enfant;
 }
+
+// void addNewGeneration(taskRepartition** currentPopulation, int nbTask, int nbProc, int** execCost, int** commCost) {
+//     int X = POPULATION_SIZE * SELECTION_RATE / 100;
+
+//     sortPopulation(currentPopulation, POPULATION_SIZE);
+
+//     taskRepartition** newPopulation = malloc(POPULATION_SIZE * sizeof(taskRepartition*));
+//     for (int i = 0; i < X; i++) {
+//         newPopulation[i] = currentPopulation[i];
+//     }
+
+//     for (int i = X; i < POPULATION_SIZE; i++) {
+//         int parent1Idx = rand() % X;
+//         int parent2Idx = rand() % POPULATION_SIZE;
+
+//         newPopulation[i] = cross(currentPopulation[parent1Idx], currentPopulation[parent2Idx], nbTask);
+//         mutate(newPopulation[i], nbTask, nbProc);
+//         // newPopulation[i]->totalCost = getTotalExecCost(newPopulation[i], execCost, commCost, nbTask);
+//         newPopulation[i]->totalCost = getTotalExecCost(newPopulation[i], execCost, commCost, nbTask, nbProc);
+
+//     }
+
+//     for (int i = 0; i < POPULATION_SIZE; i++) {
+//         if (i >= X) {
+//             free(currentPopulation[i]->procRepartition);
+//             free(currentPopulation[i]);
+//         }
+//         currentPopulation[i] = newPopulation[i];
+//     }
+
+//     free(newPopulation);
+// }
+
 
 void addNewGeneration(taskRepartition** currentPopulation, int nbTask, int nbProc, int** execCost, int** commCost) {
     int X = POPULATION_SIZE * SELECTION_RATE / 100;
@@ -279,9 +320,11 @@ void addNewGeneration(taskRepartition** currentPopulation, int nbTask, int nbPro
         int parent2Idx = rand() % POPULATION_SIZE;
 
         newPopulation[i] = cross(currentPopulation[parent1Idx], currentPopulation[parent2Idx], nbTask);
-        mutate(newPopulation[i], nbTask, nbProc);
-        // newPopulation[i]->totalCost = getTotalExecCost(newPopulation[i], execCost, commCost, nbTask);
-        newPopulation[i]->totalCost = getTotalExecCost(newPopulation[i], execCost, commCost, nbTask, nbProc);
+
+        do {
+            mutate(newPopulation[i], nbTask, nbProc);
+            newPopulation[i]->totalCost = getTotalExecCost(newPopulation[i], execCost, commCost, nbTask, nbProc);
+        } while (newPopulation[i]->totalCost == 0);
 
     }
 
@@ -295,7 +338,4 @@ void addNewGeneration(taskRepartition** currentPopulation, int nbTask, int nbPro
 
     free(newPopulation);
 }
-
-
-
 
